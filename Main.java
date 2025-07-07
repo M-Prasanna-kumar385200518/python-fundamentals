5e41d62a28cf058d4ab5652ed8338bb756653a90
M-Prasanna-kumar385200518 <bannybanny274@gmail.com>
2025-07-07 09:36:32 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    static class Student {
+        int rollno = 101;
+        String name = "ajay";
+
+        void showDetails() {
+            System.out.println("Roll No: " + rollno);
+            System.out.println("Name: " + name);
+        }
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student();         s.showDetails();              }
+}
